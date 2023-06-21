80981b6d02e3362a56c1e7e54be6cf06b361299a
Okao-Emmanuel <okaoemmanuel2@gmail.com>
2023-06-21 02:59:08 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,12 @@ int largest_number(int a, int b, int c)
 	{
 	largest = a;
 	}
-	else if (b > c)
+	else if (b >=a && b> c)
 	{
-	largest = b;
+	lsrgest = b;
 	}
 	else
 	{
 	largest = c;
 	}
-
 	return (largest);
-}
